947388c0472ac717e3a091b53a9ad4fb4a1878ae
StrongChenYu <jarvis.chenyu@gmail.com>
2020-09-19 21:54:46 +0800

    2020.9.19 完成JZ6 宣传数组的最小数字

--- src/sword_to_offer/JZ6.java
+++ src/sword_to_offer/JZ6.java
@@ -1,40 +1,39 @@
 package sword_to_offer;
 
-public class JZ6 {
-//
-//    public static void main(String[] args) {
-//        int[] array = new int[]{3,1,2};
-//        System.out.println(minNumberInRotateArray(array));
-//    }
-
     public int minNumberInRotateArray(int [] array) {
         if (array == null || array.length == 0) return 0;
-
+        
         int l = 0;
         int r = array.length - 1;
-        int mid = l + (r - l) / 2;
-
+        
         while (l < r) {
-
-            mid = l + (r - l) / 2;
-
-            if (array[l] == array[mid] && array[mid] == array[r]) return findMinInOrder(array);
-
-            if (array[mid] < array[r]) r = mid;
-            else if (array[mid] > array[l]) l = mid;
-            else if (array[mid] == array[l]) break;
-
+            int mid = l + (r - l) / 2;
+            
+            if (array[mid] == array[r] && array[mid] == array[l]) {
+                return findNumberInOrder(array);
+            }
+            
+            //等于是为了防止出现相等的情况不移动r,l指针，两者情况下都需要移动指针
+            if (array[mid] <= array[r]) {
+                r = mid;
+            } else if (array[mid] >= array[l]) {
+                l = mid;
+            } 
+            
+            if (r - l == 1) {
+                 break;
+            }
         }
-
+        
         return array[r];
     }
-
-
-    public int findMinInOrder(int [] num) {
+    
+    public int findNumberInOrder(int array[]) {
         int min = 0;
-        for (int n : num) {
-            min = Math.min(n, min);
+        for (int num : array) {
+            min = Math.min(min, num);
         }
+        
         return min;
     }
 
