981d5ac2e2c15d908f032ee1c636721d13ed5e87
chenyu <1084966084@qq.com>
2020-09-27 21:25:01 +0800

    2020.9.27 JZ14优化

--- src/sword_to_offer/JZ14.java
+++ src/sword_to_offer/JZ14.java
@@ -2,11 +2,16 @@ package sword_to_offer;
 
 import java.util.Stack;
 
+
+/**
+ *
+ 输入一个链表，输出该链表中倒数第k个结点。
+ */
 public class JZ14 {
     //这里有两个边界条件
     //1.假如k为0的边界条件，如果链表不为空，但是k为0
     //2.k的值大于实际存在的node个数，应该返回null
-    public ListNode FindKthToTail(ListNode head,int k) {
+    public ListNode FindKthToTailSlow(ListNode head,int k) {
         if (head == null || k == 0) return null;
 
         Stack<ListNode> stack = new Stack<>();
@@ -24,5 +29,31 @@ public class JZ14 {
 
         if (stack.isEmpty()) return null;
         return stack.pop();
+    }
+
+    /**
+     * 上面做法空间复杂度和时间复杂度都不如双指针。
+     */
+
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if (head == null || k == 0) return null;
+
+        ListNode l = head;
+        ListNode r = head;
+        for (int i = 0; i < k - 1; i++) {
+            if (r.next != null) {
+                r = r.next;
+            }
+            else{
+                return null;
+            }
+        }
+
+        while (r.next != null) {
+            r = r.next;
+            l = l.next;
+        }
+
+        return l;
     }
 }
