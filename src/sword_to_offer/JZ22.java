3f231d55766506d534e0850d50c2989e66633701
chenyu <1084966084@qq.com>
2020-09-30 23:32:58 +0800

    2020.9.30 提交两个比较简单的题

---
+++ src/sword_to_offer/JZ22.java
@@ -0,0 +1,47 @@
+package sword_to_offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JZ22 {
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        if (root == null) return res;
+
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.offer(root);
+
+        while (!nodes.isEmpty()) {
+            TreeNode r = nodes.poll();
+            res.add(r.val);
+
+            if (r.left != null) nodes.offer(r.left);
+            if (r.right != null) nodes.offer(r.right);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(2);
+
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        JZ22 jz22 = new JZ22();
+        ArrayList<Integer> integers = jz22.PrintFromTopToBottom(root);
+
+        for (int nums : integers) {
+            System.out.println(nums);
+        }
+
+    }
+
+}
---
+++ src/sword_to_offer/JZ38.java
@@ -0,0 +1,13 @@
+package sword_to_offer;
+
+public class JZ38 {
+
+    public int TreeDepth(TreeNode root) {
+
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+
+        return 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
+    }
+
+}
