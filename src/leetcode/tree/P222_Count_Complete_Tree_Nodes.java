b9a1522d96f05bf69398ef5b44deb02be437e7f1
chenyu <1084966084@qq.com>
2020-10-14 08:23:40 +0800

    2020.10.14 完成lc222

--- src/leetcode/tree/P222_Count_Complete_Tree_Nodes.java
+++ src/leetcode/tree/P222_Count_Complete_Tree_Nodes.java
@@ -5,6 +5,7 @@ import structure.TreeNode;
 /**
  * 计算一颗完全二叉树的节点个数
  * O(logN)时间范围内解决问题
+ * 假设是完全二叉树的全部子树全部都是二叉树
  */
 public class P222_Count_Complete_Tree_Nodes {
     public int countNodes(TreeNode root) {
@@ -30,11 +31,29 @@ public class P222_Count_Complete_Tree_Nodes {
                 cnt2++;
             }
         }
-
+        //左移运算符
         if (cnt1 == 1 && cnt2 == 1) return 1;
-        if (cnt1 == cnt2) return ((int)Math.pow(2.0, cnt1 - 1) - 1) + countNodes(nodeR) + 1;
-        if (cnt1 > cnt2) return ((int)Math.pow(2.0, cnt2 - 1) - 1) + countNodes(nodeL) + 1;
+        if (cnt1 == cnt2) return ((1 << (cnt1 - 1)) - 1) + countNodes(root.right) + 1;
+        if (cnt1 > cnt2) return ((1 << (cnt2 - 1)) - 1) + countNodes(root.left) + 1;
 
         return 0;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(0);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(0);
+        //root.left.left = new TreeNode(0);
+        //root.left.right = new TreeNode(0);
+        //root.right.left = new TreeNode(0);
+        //root.right.right = new TreeNode(0);
+
+//        root.left.left.left = new TreeNode(0);
+
+        P222_Count_Complete_Tree_Nodes p = new P222_Count_Complete_Tree_Nodes();
+
+        System.out.println(p.countNodes(null));
+
     }
 }
