f1252516b6cf27ea8e20f3d4238488dd1706b6b1
chenyu <1084966084@qq.com>
2020-10-28 11:11:41 +0800

    2020.10.28 JZ32

--- src/sword_to_offer/JZ25.java
+++ src/sword_to_offer/JZ25.java
@@ -11,7 +11,7 @@ public class JZ25 {
     //深拷贝randomnode list，左神课上所讲
     //两种方法1.借用map
     //2.放置一个虚拟节点
-    public RandomListNode Clone(RandomListNode pHead){
+    public RandomListNode Clone1(RandomListNode pHead){
         Map<RandomListNode, RandomListNode> nodeMap = new HashMap<>();
 
         //第一次先把所有的节点放进去
@@ -38,12 +38,50 @@ public class JZ25 {
     }
 
 
+    public RandomListNode Clone(RandomListNode pHead){
+        if (pHead == null) return null;
+
+        RandomListNode head = pHead;
+
+        while (head != null) {
+            RandomListNode copyNode = new RandomListNode(head.label);
+            copyNode.next = head.next;
+            head.next = copyNode;
+            head = copyNode.next;
+        }
+
+        head = pHead;
+        while (head != null) {
+            if (head.random != null) head.next.random = head.random.next;
+            head = head.next.next;
+        }
+
+        head = pHead;
+        RandomListNode res = pHead.next;
+        RandomListNode tempNode;
+        while (head != null) {
+            tempNode = head.next;
+            head.next = tempNode.next;
+
+            if (tempNode.next != null) {
+                tempNode.next = tempNode.next.next;
+            }
+
+            head = head.next;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         RandomListNode node = new RandomListNode(1);
         node.next = new RandomListNode(2);
+
         node.next.next = new RandomListNode(3);
         node.next.next.next = new RandomListNode(4);
         node.next.next.next.next = new RandomListNode(5);
+
+        node.random = node.next.next.next;
 
         JZ25 jz25 = new JZ25();
 
--- src/sword_to_offer/JZ32.java
+++ src/sword_to_offer/JZ32.java
@@ -1,13 +1,42 @@
 package sword_to_offer;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+//主要是贪心的思想
 public class JZ32 {
 
     public String PrintMinNumber(int [] numbers) {
-        return "";
+        if (numbers == null || numbers.length == 0) return "";
+        String[] ns = new String[numbers.length];
+
+        for (int i = 0; i < ns.length; i++) {
+            ns[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(ns, new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return (s + t1).compareTo(t1 + s);
+            }
+        });
+
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : ns) {
+            sb.append(s);
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println("341".compareTo("3"));
+        int[] numbers = new int[]{3,32,321,1,2,3,4};
+
+        JZ32 jz32 = new JZ32();
+
+        System.out.println(jz32.PrintMinNumber(numbers));
+
     }
 
 }
