1d4a2ae25b074a2e2ce4cf40ef5711494798884c
chenyu <1084966084@qq.com>
2020-11-01 21:40:46 +0800

    2020.11.1 complete JZ24

--- src/sword_to_offer/JZ24.java
+++ src/sword_to_offer/JZ24.java
@@ -4,6 +4,10 @@ import structure.TreeNode;
 
 import java.util.ArrayList;
 
+
+//输入一颗二叉树的根节点和一个整数，
+// 按字典序打印出二叉树中结点值的和为输入整数的所有路径。
+// 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
 public class JZ24 {
 
     public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
@@ -16,6 +20,7 @@ public class JZ24 {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 
         if (val == target) {
+            //只有页节点的时候才可以进行合并
             if (root.left == null && root.right == null) {
                 ArrayList<Integer> temp = new ArrayList<>();
                 temp.add(val);
