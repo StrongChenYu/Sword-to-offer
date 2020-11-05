2ce603e8c2ea506675efe5d14001d887295d8ea7
chenyu <1084966084@qq.com>
2020-11-05 21:46:06 +0800

    2020.11.5 complete JZ37

--- src/sword_to_offer/JZ37.java
+++ src/sword_to_offer/JZ37.java
@@ -6,17 +6,13 @@ public class JZ37 {
     public int GetNumberOfK(int [] array , int k) {
         if (array == null || array.length == 0) return 0;
 
-        int l = GetLeft(array, k);
-        int r = GetLeft(array, k + 1);
+        int l = lowBound(array, k);
+        int r = lowBound(array, k + 1);
 
-        if (array[r] == k + 1){
-
-        }
-
-        return 0;
+        return r - l;
     }
 
-    public int GetLeft(int[]array, int k) {
+    public int lowBound(int[]array, int k) {
         int left = 0;
         int right = array.length;
         int mid;
@@ -36,9 +32,9 @@ public class JZ37 {
     public static void main(String[] args) {
         JZ37 jz37 = new JZ37();
 
-        int[] a = new int[]{1,2,3,4,4,6};
+        int[] a = new int[]{1,2,3,4,4,5};
 
-        System.out.println(jz37.GetLeft(a, 5));
+        System.out.println(jz37.lowBound(a, 5));
         System.out.println(4 >> 1);
     }
 }
