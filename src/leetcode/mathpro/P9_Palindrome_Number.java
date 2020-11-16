af38c4e4a9653f667ddfca13d63e95d161d2372e
chenyu <1084966084@qq.com>
2020-11-16 21:57:27 +0800

    2020.11.16 complete leetcode p9

--- src/leetcode/mathpro/P9_Palindrome_Number.java
+++ src/leetcode/mathpro/P9_Palindrome_Number.java
@@ -3,7 +3,7 @@ package leetcode.mathpro;
 public class P9_Palindrome_Number {
 
     //bad solution
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome1(int x) {
         if (x < 0) return false;
 
         String s = String.valueOf(x);
@@ -17,8 +17,20 @@ public class P9_Palindrome_Number {
         return true;
     }
 
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int tempR = 0;
+        while (tempR < x) {
+            tempR = tempR * 10 + (x % 10);
+            x = x / 10;
+        }
+
+        return tempR == x || tempR / 10 == x;
+    }
+
     public static void main(String[] args) {
         P9_Palindrome_Number p = new P9_Palindrome_Number();
-        System.out.println(p.isPalindrome(0));
+        System.out.println(p.isPalindrome(123321));
     }
 }
