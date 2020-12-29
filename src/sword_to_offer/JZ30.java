4c1ed231aef48a8cca3e341e26cd906895ef9c58
chenyu <1084966084@qq.com>
2020-12-29 08:45:29 +0800

    2020.12.29 JZ30 DP solution

--- src/sword_to_offer/JZ30.java
+++ src/sword_to_offer/JZ30.java
@@ -6,6 +6,34 @@ public class JZ30 {
     private final int LEFT = 1;
     private final int RIGHT = 2;
 
+
+    /**
+     * 核心思想就在于，如果先直接用迭代的方式解决的话，时间复杂度很高
+     * 优化：使用二维数组保存i-j之间的和，以防止重复计算
+     *
+     * 更简单的思想在于，保存以dp[i]描述的是：
+     * ****以num[i]为结尾，连续子树组的最大值
+     * @param array
+     * @return
+     */
+    public int FindGreatestSumOfSubArrayDP(int[] array) {
+        if (array == null || array.length == 0) return 0;
+        int n = array.length;
+
+        int[] status = new int[n];
+        status[0] = array[0];
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            status[i] = Math.max(array[i], status[i - 1] + array[i]);
+            res = Math.max(res, status[i]);
+        }
+
+        return res;
+    }
+
+
+
+
     /**
      * 分治解决，其实就是分开递归
      * @param array
