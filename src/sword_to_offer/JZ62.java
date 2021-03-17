69e5854521b7dbbf05b1e6f101c32930003a5974
chenyu <1084966084@qq.com>
2021-03-17 21:00:42 +0800

    2021.3.17 JZ62 easy but not finish

---
+++ src/sword_to_offer/JZ62.java
@@ -0,0 +1,44 @@
+package sword_to_offer;
+
+import org.junit.Before;
+import org.junit.Test;
+import structure.TreeNode;
+
+/**
+ * @Author Chen Yu
+ * @Date 2021/3/17 20:23
+ */
+public class JZ62 {
+
+    /**
+     * 前序遍历的第k个节点
+     * 用栈模拟递归的过程
+     * @param pRoot
+     * @param k
+     * @return
+     */
+    TreeNode KthNode(TreeNode pRoot, int k) {
+
+    }
+
+    TreeNode testRoot;
+
+    @Before
+    public void generateTree() {
+        testRoot = new TreeNode(5);
+        testRoot.left = new TreeNode(3);
+        testRoot.right = new TreeNode(7);
+
+        testRoot.left.left = new TreeNode(2);
+        testRoot.left.right = new TreeNode(4);
+
+        testRoot.right.left = new TreeNode(6);
+        testRoot.right.right = new TreeNode(8);
+    }
+
+    @Test
+    public void Test() {
+
+        KthNode(testRoot, 3);
+    }
+}
