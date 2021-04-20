55f31f655e723b1199a3ba6a79497cd135d29b2d
chenyu <1084966084@qq.com>
2021-04-20 09:32:40 +0800

    2021.4.20 leetcode p23 stupid solution

---
+++ src/leetcode/linkedlist/P23_Merge_k_Sorted_Lists.java
@@ -0,0 +1,14 @@
+package leetcode.heap;
+
+import structure.ListNode;
+
+/**
+ * @Author Chen Yu
+ * @Date 2021/4/20 9:25
+ */
+public class P23_Merge_k_Sorted_Lists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+    }
+}
