2ff696df4e0f5db8d6944f3a92d38485564994bf
chenyu <1084966084@qq.com>
2021-04-20 18:53:38 +0800

    2021.4.20 leetcode p23 simplify code

--- src/leetcode/linkedlist/P23_Merge_k_Sorted_Lists.java
+++ src/leetcode/linkedlist/P23_Merge_k_Sorted_Lists.java
@@ -1,4 +1,4 @@
-package leetcode.heap;
+package leetcode.linkedlist;
 
 import structure.ListNode;
 
@@ -9,6 +9,31 @@ import structure.ListNode;
 public class P23_Merge_k_Sorted_Lists {
 
     public ListNode mergeKLists(ListNode[] lists) {
-        
+        ListNode res = null;
+
+        for (int i = 0; i < lists.length; i++) {
+            res = mergeTwoLists(res, lists[i]);
+        }
+
+        return res;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(-1);
+        ListNode head = res;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                res.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                res.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            res = res.next;
+        }
+
+        res.next = l1 != null ? l1 : l2;
+        return head.next;
     }
 }
