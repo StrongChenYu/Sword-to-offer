cd2f461a99f377b7a2473eacf27cff908a2df2ca
chenyu <1084966084@qq.com>
2021-04-24 21:47:02 +0800

    2021.4.24 leetcode p135

---
+++ src/leetcode/greedy/P135_Candy.java
@@ -0,0 +1,26 @@
+package leetcode.greedy;
+
+import java.util.Arrays;
+
+/**
+ * @Author Chen Yu
+ * @Date 2021/4/24 20:29
+ */
+public class P135_Candy {
+
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length];
+        int[] right = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for(int i = 1; i < ratings.length; i++)
+            if(ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+        int count = left[ratings.length - 1];
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1]) right[i] = right[i + 1] + 1;
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
+    }
+
+}
