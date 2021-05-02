ab35f1c406b1eeb82668f442686cabdafdfc0e05
chenyu <1084966084@qq.com>
2021-05-02 21:33:28 +0800

    2021.5.2 leetcode p98

--- src/leetcode/tree/P98_Validate_Binary_Search_Tree.java
+++ src/leetcode/tree/P98_Validate_Binary_Search_Tree.java
@@ -5,21 +5,18 @@ import structure.TreeNode;
 public class P98_Validate_Binary_Search_Tree {
 
     public boolean isValidBST(TreeNode root) {
-        return isValidBSTHelp(root, null, null);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean isValidBSTHelp(TreeNode root, Integer lower, Integer upper) {
-        if (root == null) return true;
-
-        int val = root.val;
-        if (lower != null && val <= lower) return false;
-        if (upper != null && val >= upper) return false;
-
-        if (!isValidBSTHelp(root.left, lower, val)) return false;
-        if (!isValidBSTHelp(root.right, val, upper)) return false;
-
-        return true;
-
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
     }
+
 
 }
