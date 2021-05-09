4b38b71d63c61e225f381fe6ce61b3a907cd684f
chenyu <1084966084@qq.com>
2021-05-09 17:01:01 +0800

    2021.5.9 leetcode p124

--- src/leetcode/tree/P124_Binary_Tree_Maximum_Path_Sum.java
+++ src/leetcode/tree/P124_Binary_Tree_Maximum_Path_Sum.java
@@ -8,8 +8,21 @@ import structure.TreeNode;
  */
 public class P124_Binary_Tree_Maximum_Path_Sum {
 
+    private int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        return 0;
+        maxSum(root);
+        return max;
+    }
+
+    public int maxSum(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = Math.max(maxSum(root.left), 0);
+        int right = Math.max(maxSum(root.right), 0);
+
+        max = Math.max(max, left + root.val + right);
+
+        return Math.max(left, right) + root.val;
     }
 
 }
