2c24b1834ec8afbd1c6068740f6e3d842e64fd89
chenyu <1084966084@qq.com>
2021-09-17 08:27:43 +0800

    feat(dfs): leetcode p39

---
+++ src/leetcode/dfs/P39_Combination_Sum.java
@@ -0,0 +1,51 @@
+package leetcode.dfs;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class P39_Combination_Sum {
+
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) {
+            return res;
+        }
+
+        dfs(candidates, target, 0);
+        return res;
+    }
+
+    public void dfs(int[] candidates, int target, int begin) {
+
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        boolean[] visited = new boolean[candidates.length];
+        for (int i = begin; i < candidates.length; i++) {
+            if (!visited[i]) {
+
+                path.add(candidates[i]);
+                int left = target - candidates[i];
+
+                if (left >= 0) {
+                    dfs(candidates, left, i);
+                }
+
+                path.remove(path.size() - 1);
+                visited[i] = true;
+            }
+        }
+    }
+
+
+
+    @Test
+    public void Test() {
+        combinationSum(new int[]{2,3,6,7}, 7);
+    }
+}
