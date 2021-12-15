2e25f9b61e60cecd49d8b54de41eaf2db74c0297
chenyu <1084966084@qq.com>
2021-12-15 20:50:52 +0800

    feat(array): leetcode p240

---
+++ src/leetcode/array/P240_Search_a_2D_Matrix_II.java
@@ -0,0 +1,20 @@
+package leetcode.array;
+
+public class P240_Search_a_2D_Matrix_II {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
