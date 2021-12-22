09ef0a15e530cccf2dc910e2ea1d37efbeb148de
chenyu <1084966084@qq.com>
2021-12-22 19:25:19 +0800

    feat(dp): leetcode p300 dp with binary search

--- src/leetcode/dynamicproblem/P300_Longest_Increasing_Subsequence.java
+++ src/leetcode/dynamicproblem/P300_Longest_Increasing_Subsequence.java
@@ -5,22 +5,32 @@ import org.junit.Test;
 public class P300_Longest_Increasing_Subsequence {
     // O(N^2) solution
     public int lengthOfLIS(int[] nums) {
+        //dp 记录当前最长的子序列是什么
         int[] dp = new int[nums.length];
-
-        int max = 0;
+        //r 记录当前最长的子序列的长度
+        int res = 0;
         for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-            for (int j = 0; j <= i; j++) {
-                dp[i] = nums[j] < nums[i] ? Math.max(dp[i], dp[j] + 1) : dp[i];
+            int l = 0;
+            int r = res;
+            while (l < r) {
+                int middle = ((r - l) >> 1) + l;
+                if (dp[middle] < nums[i]) {
+                    l = middle + 1;
+                } else {
+                    r = middle;
+                }
             }
-            max = Math.max(max, dp[i]);
+            dp[l] = nums[i];
+            if (l == res) {
+                res++;
+            }
         }
 
-        return max;
+        return res;
     }
 
     @Test
     public void Test() {
-        System.out.println(lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(lengthOfLIS(new int[]{0,1,0,3,2,3}));
     }
 }
