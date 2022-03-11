41748b1f31d693551a5df34b1630ddcf593b192c
chenyu <1084966084@qq.com>
2022-03-11 12:59:26 +0800

    feat(dp): leetcode p120

--- src/leetcode/dynamicproblem/P120_Triangle.java
+++ src/leetcode/dynamicproblem/P120_Triangle.java
@@ -1,5 +1,9 @@
 package leetcode.dynamicproblem;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,35 +14,47 @@ public class P120_Triangle {
 
     public int minimumTotal(List<List<Integer>> triangle) {
 
-        int rows = triangle.size();
-        int cols = triangle.get(rows - 1).size();
+        int n = triangle.size();
+        int m = triangle.get(n - 1).size();
 
-        int[] dp = new int[cols];
+        int[] dp = new int[m];
 
-        dp[0] = triangle.get(0).get(0);
-
-        int res = dp[0];
-        for (int i = 1; i < rows; i++) {
-            List<Integer> curLevel = triangle.get(i);
-            int curSize = curLevel.size();
-
-            int tempMin = Integer.MAX_VALUE;
-            for (int j = curSize - 1; j >= 0; j--) {
-                int curValue = curLevel.get(j);
+        for (int i = 0; i < n; i++) {
+            List<Integer> curRow = triangle.get(i);
+            for (int j = i; j >= 0; j--) {
+                int curNum = curRow.get(j);
 
                 if (j == 0) {
-                    dp[j] = dp[j] + curValue;
-                } else if(j == curSize - 1) {
-                    dp[j] = dp[j - 1] + curValue;
-                } else {
-                    dp[j] = Math.min(dp[j], dp[j - 1]) + curValue;
+                    dp[j] = dp[j] + curNum;
+                    continue;
                 }
 
-                tempMin = Math.min(tempMin, dp[j]);
+                if (j == i) {
+                    dp[j] = dp[j - 1] + curNum;
+                    continue;
+                }
+
+                dp[j] = Math.min(dp[j] + curNum, dp[j - 1] + curNum);
             }
-            res = tempMin;
+        }
+
+        int res = Integer.MAX_VALUE;
+        for (int i : dp) {
+            res = Math.min(res, i);
         }
 
         return res;
+    }
+
+    @Test
+    public void test() {
+        List<List<Integer>> data = new ArrayList<>();
+
+        data.add(Arrays.asList(2));
+        data.add(Arrays.asList(3,4));
+        data.add(Arrays.asList(6,5,7));
+        data.add(Arrays.asList(4,1,8,3));
+
+        minimumTotal(data);
     }
 }
