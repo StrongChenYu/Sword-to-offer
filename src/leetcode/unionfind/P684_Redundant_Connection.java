52e371c3f9258981246b4f8e85005e31a0de3c51
chenyu <1084966084@qq.com>
2022-07-15 10:48:49 +0800

    feat(unionfind): leetcode p684

--- src/leetcode/unionfind/P684_Redundant_Connection.java
+++ src/leetcode/unionfind/P684_Redundant_Connection.java
@@ -2,65 +2,63 @@ package leetcode.unionfind;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @Author Chen Yu
  * @Date 2021/10/22 23:47
  */
 public class P684_Redundant_Connection {
 
+
     public int[] findRedundantConnection(int[][] edges) {
-        UnionFind unionFind = new UnionFind(edges.length + 1);
+        UF uf = new UF(1001);
 
         for (int i = 0; i < edges.length; i++) {
             int e1 = edges[i][0];
             int e2 = edges[i][1];
-            if (unionFind.isConnect(e1, e2)) {
-                return new int[]{edges[i][0], edges[i][1]};
+
+            if (uf.isConnected(e1, e2)) {
+                return edges[i];
             }
-            unionFind.union(e1, e2);
+
+            uf.union(e1, e2);
         }
-        return null;
+
+        return new int[]{};
     }
 
-    public static class UnionFind {
+    private static class UF {
         int[] parent;
 
-        public UnionFind(int nodeNums) {
-            parent = new int[nodeNums];
-            for (int i = 0; i < nodeNums; i++) {
+        public UF(int n) {
+            parent = new int[n + 1];
+
+            for (int i = 0; i <= n; i++) {
                 parent[i] = i;
             }
-        }
-
-        public int find(int node) {
-            int par = parent[node];
-            while(par != parent[par]) {
-                parent[par] = parent[parent[par]];
-                par = parent[par];
-            }
-            return par;
         }
 
         public void union(int node1, int node2) {
             int root1 = find(node1);
             int root2 = find(node2);
 
-            if (root1 != root2) {
-                parent[root1] = root2;
+            if (root1 == root2) {
+                return;
             }
+
+            parent[root1] = root2;
         }
 
-        public boolean isConnect(int node1, int node2) {
+        public int find(int node) {
+            if (node != parent[node]) {
+                parent[node] = find(parent[node]);
+            }
+            return parent[node];
+        }
+
+        public boolean isConnected(int node1, int node2) {
             return find(node1) == find(node2);
         }
-    }
-
-    int[][] test = {
-            {1,2}, {1,3}, {2,3}
-    };
-
-    @Test
-    public void Test() {
-        findRedundantConnection(test);
     }
 }
